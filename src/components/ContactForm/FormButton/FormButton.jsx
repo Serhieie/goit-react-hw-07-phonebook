0f5884f2f42a4c183793253d177efb46cdc5b81b
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-12-26 11:24:52 +0200

    text-balanse

--- src/components/ContactForm/FormButton/FormButton.jsx
+++ src/components/ContactForm/FormButton/FormButton.jsx
@@ -11,7 +11,7 @@ export const FormButton = ({ isLoading, isThemeDark }) => {
           ? 'shadow-none hover:bg-buttonHoverColorDark text-buttonTextColorDark hover:text-lightPartsColorDark bg-buttonColorDark'
           : 'shadow-shadowBox hover:bg-buttonHoverColor text-buttonTextColor bg-buttonColor'
       } text-center font-light w-40 h-11 rounded-sm border-none outline-none mx-auto cursor-pointer shadow-md flex items-center justify-around transition-all duration-30 ${
-        isThemeDark ? 'text-lg md:w-48' : 'text-md md:w-40'
+        isThemeDark ? 'text-lg md:w-48' : 'text-lg md:w-48'
       } ssm:w-40 ssm:h-10 md2:text-sm`}
     >
       {isLoading ? (
